830494354d7be7bb0489b3e4bb852a3193f3540f
Aabhash Tiwari <35108630+Aabhash97@users.noreply.github.com>
2023-03-01 14:02:57 +0000

    fixed the row component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,15 +5,18 @@ import requests from '../Request';
 
 const Home = () => {
   //console.log(requests);
+  const re = {
+    title : "popular",
+    fetchURL : "https://api.themoviedb.org/3/movie/popular?api_key=fb0410526e6ca523d5e6fbbf4908413b"
+  }
   return (
     <>
       <Main />
       {requests.map((request) => {
         console.log(request);
-        <Row title={request.title} fetchURL={request.fetchURL} />
+        return (<Row title={request.title} fetchURL={request.fetchURL} />)
         console.log("Row component called");
-      })}
-    </>
+      })}    </>
   );
 }
 
