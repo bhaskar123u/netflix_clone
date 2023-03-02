ac5c9a412e29f2f088cb0167aa8c344ca2ce9d21
Bhaskar Sharan <bhaskar123u@gmail.com>
2023-03-02 11:01:44 +0530

    Committing changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,16 +9,9 @@ const Home = () => {
     <>
       <Main />
       {requests.map((request) => {
-<<<<<<< HEAD
-        return <Row key={request.title} title={request.title} fetchURL={request.fetchURL} />
+        return (<Row title={request.title} fetchURL={request.fetchURL} />)
       })}
     </>
-=======
-        console.log(request);
-        return (<Row title={request.title} fetchURL={request.fetchURL} />)
-        console.log("Row component called");
-      })}    </>
->>>>>>> ea03f5668264099ab628f6ee00f093de89ef1226
   );
 }
 
